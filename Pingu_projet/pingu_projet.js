8e0f48348420052a5656c63329358459aedb1ac9
Belpois Nicolas <nicolasbelpois@gmail.com>
2017-10-27 16:54:55 +0200

    ajout d'un petit vrai ou faux

--- Pingu_projet/pingu_projet.js
+++ Pingu_projet/pingu_projet.js
@@ -3,6 +3,9 @@ var text_descriptif = document.querySelectorAll('.photo');
 var where_to_show = document.querySelector('.alltext');
 var selecteP = "";
 var main_select = document.querySelector(".main").offsetWidth;
+var launch = document.querySelector('#fat-blue');
+var reponse1 = "VRAI";
+var reponse2 = "FAUX";
 
 
 req.open('get', 'data.json', true);
@@ -19,4 +22,26 @@ req.onload = function (event) {
     })
 
   })
+}
+
+launch.addEventListener('click', function(){
+  alert("Bienvenue")
+  alert("Ce jeu est un vrai ou faux, ce qui signifie que seul les réponses Vrai ou Faux sont accepter.")
+  confirm("Si tu est prêt à jouer, click sur le boutton Ok.")
+  Guessrep();
+})
+function Guessrep() {
+  var rep = prompt("La masse du Manchot Empereure ce situe entre 20Kg et 40kg.")
+  rep = rep.toUpperCase();
+  if (rep === reponse1) {
+    alert("Bravo tu est très intlligent ! Tu doit surement être un expert en Pingouin renomée dans le monde entier !!!!")
+
+  }else if (rep === reponse2) {
+    alert("Hahahahahahaha")
+    alert("hahaha hahaha")
+    alert("haaaaaaa..... tu est trop nul !")
+
+  }else if (rep != reponse1 && reponse2) {
+    alert("Ont a dit Vrai !! ou FAUX !!")
+  }
 }
